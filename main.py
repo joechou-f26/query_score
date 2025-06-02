4b4d07e0d36791780d40fe140e8f6d18e8c13cd9
joechou-f26 <71837456+joechou-f26@users.noreply.github.com>
2025-06-02 13:29:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,11 +40,13 @@ if st.button("查詢"):
             score_only = student_row.drop(columns=['學號', '姓名','期中考'], errors='ignore')  #只做小考平均
             score_only = score_only.apply(pd.to_numeric, errors='coerce')
             avg_score = score_only.mean(axis=1).round(2)
-
+            cnt_score=len(score_only)
+            
             # 加上平均分數欄（放在第 2 個欄位，也就是 學號 和 姓名 之後）
             student_row = student_row.copy()
             student_row.insert(2, '小考平均', avg_score)  # index=2 表示放在第 3 欄
-
+            student_row.insert(3, '小考次數', cnt_score)  
+            
             # 顯示成績
             st.subheader("🔎 查詢結果")
             st.dataframe(student_row)
